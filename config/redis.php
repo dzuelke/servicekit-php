c62e92c9dad1218e3be660e818e1f9806297abec
David Zuelke <dzuelke@gmail.com>
2016-02-20 23:01:12 +0100

    try multiple redis env vars

--- config/redis.php
+++ config/redis.php
@@ -6,9 +6,15 @@
  * the values are read from the environment variables, and there is no need to change this file.
  * -----------------------------------------------------------------------------------------------*/
 
+foreach(array('REDIS_URL', 'REDISCLOUD_URL', 'REDISGREEN_URL', 'REDISTOGO_URL', 'OPENREDIS_URL') as $var) {
+  $url = getenv($var);
+  if($url) break;
+}
+$url = parse_url($url);
+
 return array(
-  'host' => parse_url(getenv('REDISTOGO_URL'), PHP_URL_HOST),
-  'port' => parse_url(getenv('REDISTOGO_URL'), PHP_URL_PORT),
-  'password' => parse_url(getenv('REDISTOGO_URL'), PHP_URL_PASS),
+  'host' => $url['host'],
+  'port' => $url['port'],
+  'password' => $url['pass'],
   'prefix' => 'servicekit'
 );
